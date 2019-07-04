fc589554a56e713ee650737e6d21e96d3005b480
rpenumalli <rpenumalli@Livevox.onmicrosoft.com>
2019-07-04 16:46:42 +0530

    first commit

---
+++ src/main/java/com/livevox/Calculator.java
@@ -0,0 +1,19 @@
+package com.livevox;
+
+public class Calculator {
+    public int add(int a,int b){
+          return (a+b);
+    }
+    
+    public int multi(int a,int b){
+        return (a*b);
+  }
+    
+    public int sub(int a,int b){
+        return (a-b);
+  }
+    
+    public int div(int a,int b){
+        return (a/b);
+  }
+}
---
+++ src/test/java/com/livevox/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.livevox;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+	boolean isTestExpectedToPass=true;
+
+	public void testAdd() {
+		assertTrue(isTestExpectedToPass);
+	}
+
+//	public void testMulti() {
+//		assertTrue(isTestExpectedToPass);
+//	}
+//
+//	public void testSub() {
+//		assertTrue(isTestExpectedToPass);
+//	}
+//
+//	public void testDiv() {
+//		assertTrue(isTestExpectedToPass);
+//	}
+
+}
